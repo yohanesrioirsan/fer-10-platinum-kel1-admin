d9bffde6d5db483fd096105b6dd761b0a211c79a
yohanesrioirsan <rioirsan8@gmail.com>
2023-06-11 11:25:24 +0700

    for admin page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./App.css";
+
+function App() {
+  return (
+    <Container>
+      <h1>Admin Page</h1>
+    </Container>
+  );
+}
+
+export default App;
