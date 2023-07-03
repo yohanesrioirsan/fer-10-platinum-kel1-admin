20b01fe1bfd353c1cb5a15ecc48eee21e79b7161
Naufal Ariful <naufalariful16@gmail.com>
2023-07-03 23:28:54 +0700

    table in dashboard

---
+++ src/containers/DashboardPagination.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { FormControl } from "react-bootstrap";
+import { PaginationControl } from "react-bootstrap-pagination-control";
+import { useDispatch, useSelector } from "react-redux";
+import { selectTableValue, setPage, setPageSize } from "../redux/tableSlice";
+
+export default function DashboardPagination() {
+    const table = useSelector(selectTableValue)
+    const dispatch = useDispatch();
+
+    const onLimitChange = (e) => {
+        dispatch(setPageSize(e.target.value))
+    }
+
+    return (
+        <div>
+            <div className="d-flex justify-content-between align-items-start">
+                <div className="d-flex flex-grow-1">
+                    <div className="me-3">
+                        <span className="d-block">Limit</span>
+                        <FormControl as="select"  onChange={onLimitChange}>
+                            <option value="10">10</option>
+                            <option value="50">50</option>
+                            <option value="100">100</option>
+                        </FormControl>
+                    </div>
+                    {table && table.table.pageCount && table.table.pageCount > 0 ? (
+                        <div>
+                            <span className="d-block">Jump to page</span>
+                            <FormControl as="select" onChange={(e) => dispatch(setPage(e.target.value))}>
+                                {Array.from(
+                                    { length: Math.ceil(table.table.pageCount / table.table.pageSize) },
+
+                                    (_, index) => (
+                                        <option value={index + 1} key={index}>
+                                            {index + 1}
+                                        </option>
+                                    )
+                                )}
+                            </FormControl>
+                        </div>
+                    ) : null}
+                </div>
+                <div>
+                    <span className="d-block">&nbsp;</span>
+                    <PaginationControl
+                        page={1}
+                        between={4}
+                        total={table && table.table.pageCount ? table.table.pageCount : 0}
+                        limit={table && table.table.pageSize ? table.table.pageSize : 0}
+                        changePage={(p) => {
+                            dispatch(setPage(p));
+                        }}
+                        ellipsis={1}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ src/containers/Index.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import DashboardTable from "./DashboardTable";
+import DashboardPagination from "./DashboardPagination";
+import { selectTablePageSizeValue, selectTablePageValue, selectTableValue, setTable } from "../redux/tableSlice";
+
+export default function DashboardContainer() {
+    const pageSize = useSelector(selectTablePageSizeValue)
+    const page = useSelector(selectTablePageValue)
+
+    const dispatch = useDispatch();
+    const fetchAPIOrder = async () => {
+        try {
+            const response = await axios.get("https://bootcamp-rent-cars.herokuapp.com/admin/v2/order", {
+                params: {
+                    page,
+                    pageSize,
+                },
+                headers: {
+                    access_token:
+                        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGJjci5pbyIsInJvbGUiOiJBZG1pbiIsImlhdCI6MTY4ODM5Mjc2MH0.5Swm1H97WL0nEm1Gu5T5rJqUe_Jx9XP4IGQNikdDJ5k",
+                },
+            });
+
+            console.log(response);
+            if (response.status === 200) {
+                console.log(response.data)
+                dispatch(setTable(response.data));
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchAPIOrder();
+    }, [page, pageSize]);
+
+    return (
+        <div className="p-5">
+            <h1>Dashboard</h1>
+            <h1>{page}</h1>
+            <DashboardTable />
+            <DashboardPagination />
+        </div>
+    );
+}
